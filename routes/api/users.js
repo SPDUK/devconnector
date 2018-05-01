d28606f05b787233a166a6c9d84d58a82a79372c
spdev <spdevuk@gmail.com>
2018-05-01 01:52:00 +0100

    added user schema and user model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const gravatar = require('gravatar');
 
 const router = express.Router();
 
+// Load User model
+const User = require('../../models/User');
 // @route GET api/users/test
 // @desc Tests users route
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
+
+// @route GET api/users/register
+// @desc Register user
+// @access Public
+
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists' });
+    }
+    const avatar = gravatar.url(req.body.email, {
+      s: 200, // Size
+      r: 'pg', // Rating
+      d: 'mm' // Default Image
+    });
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      avatar,
+      password: req.body.password
+    });
+  });
+});
 
 module.exports = router;
