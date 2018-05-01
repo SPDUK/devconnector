2a9bbc3b220ad40544bf34b12d8bce1dd9ab1119
spdev <spdevuk@gmail.com>
2018-05-01 06:23:08 +0100

    added currentuser route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -21,8 +22,11 @@ mongoose
   .then(() => console.log('mongoDB connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('hello'));
+// Passport middleware
+app.use(passport.initialize());
 
+// Passport config
+require('./config/passport')(passport);
 // use routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 const router = express.Router();
 
@@ -82,5 +83,20 @@ router.post('/login', (req, res) => {
     });
   });
 });
+
+// @route GET api/users/current
+// @desc Return current User
+// @access Private
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
