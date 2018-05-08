5b545107a59e90a8ccebedabee78ebd29b0b797c
Steve <spdevuk@gmail.com>
2018-05-08 05:15:17 +0100

    added comment

--- validation/login.js
+++ validation/login.js
@@ -19,7 +19,8 @@ module.exports = function validateLoginInput(data) {
   if (Validator.isEmpty(data.password)) {
     errors.password = 'Password field is required';
   }
-
+  // returns an object with two properties errors and isValid, isEmpty has it's own properties such as errors.email
+  // errors.email = "email is invalid"
   return {
     errors,
     isValid: isEmpty(errors)
