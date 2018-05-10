aa601e178155bf329ba7c9427f9f0577d69435fc
Steve <spdevuk@gmail.com>
2018-05-10 16:25:43 +0100

    eslint not important

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import setAuthToken from '../utils/setAuthToken';
 import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
 import { GET_ERRORS, SET_CURRENT_USER } from './types';
 // Register user
 export const registerUser = (userData, history) => dispatch => {
