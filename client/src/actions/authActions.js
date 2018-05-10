0bdd0fff9bf9ef6f4412c07b0f883ac0f62a18f2
Steve <spdevuk@gmail.com>
2018-05-10 15:38:52 +0100

    added setAuthToken file to add auth token on requests with axios

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -12,3 +12,23 @@ export const registerUser = (userData, history) => dispatch => {
       })
     );
 };
+
+// login - get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      // save to localStorage
+      const { token } = res.data;
+      // set token to localStorage
+      localStorage.setItem('jwtToken', token);
+      // set token to auth header
+      setAuthToken(token);
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
