e4c79f7187e52c87bd529e53e802a8790e99edce
Steve <spdevuk@gmail.com>
2018-05-14 13:37:46 +0100

    fixed typo

--- client/src/components/profiles/Profiles.jsx
+++ client/src/components/profiles/Profiles.jsx
@@ -25,7 +25,7 @@ class Profiles extends Component {
           <div className="row">
             <div className="col-md-12">
               <h1 className="display-4 text-center">Developer Profiles</h1>
-              <p className="lead text-cetner">Browse and connect with developers</p>
+              <p className="lead text-center">Browse and connect with developers</p>
               {profileItems}
             </div>
           </div>
