ddccdc55162b66d1dbafda6add449af9bb7e3b36
Steve <spdevuk@gmail.com>
2018-05-14 16:23:11 +0100

    added social links & https to make inputs work for "www.website.com" not needing http:// on input

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -31,7 +31,7 @@ class Profile extends Component {
             </div>
             <div className="col-md-6" />
           </div>
-          <ProfileHeader />
+          <ProfileHeader profile={profile} />
           <ProfileAbout />
           <ProfileCreds />
           <ProfileGithub />
