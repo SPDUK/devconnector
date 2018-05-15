36e5cd58abf941ca603f0eb095fef0cca513cf50
Steve <spdevuk@gmail.com>
2018-05-15 10:19:56 +0100

    Fixed small error changed expItems to eduItems

--- client/src/components/profile/ProfileCreds.jsx
+++ client/src/components/profile/ProfileCreds.jsx
@@ -75,7 +75,7 @@ class ProfileCreds extends Component {
         <div className="col-md-6">
           <h3 className="text-center text-info">Education</h3>
           {eduItems.length > 0 ? (
-            <ul className="list-group">{expItems}</ul>
+            <ul className="list-group">{eduItems}</ul>
           ) : (
             <p className="text-center">No Education Listed</p>
           )}
